a32ef69abb3d6b299f5589cda3ffa423785ea3fd
deadjakk <deadjakk@shell.rip>
2021-04-02 16:24:39 -0500

    added script for getting sha hash

---
+++ android/get-sha-hash-for-certpinning.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo 'script.sh <*.domain.com> <domain.com>'
+openssl s_client -servername $1 -connect $2:443 | openssl x509 -pubkey -noout | openssl pkey -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64
