3aacb8ee57820b072a0d7c64e5f1952037a8f89a
deadjakk <deadjakk@shell.rip>
2021-04-02 16:25:29 -0500

    credit

--- android/get-sha-hash-for-certpinning.sh
+++ android/get-sha-hash-for-certpinning.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# thnx 2 @ac3lives
 echo 'script.sh <*.domain.com> <domain.com>'
 openssl s_client -servername $1 -connect $2:443 | openssl x509 -pubkey -noout | openssl pkey -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64
