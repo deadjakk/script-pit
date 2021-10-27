77f385e99ec4e3ff607f3fcad679a3d7f98693a9
deadjakk <deadjakk@shell.rip>
2021-10-26 19:16:32 -0500

    added more queries

--- ldapdomaincustom/queries.py
+++ ldapdomaincustom/queries.py
@@ -8,5 +8,20 @@ queries={
     "name":"all users, all properties",
     "filter":"(objectClass=user)",
     "properties":"*"
+    },
+    3:{
+    "name":"exchange servers",
+    "filter":"(&(objectCategory=computer)(servicePrincipalName=exchangeMDB*))",
+    "properties":"*"
+    },
+    4:{
+    "name":"unconstrained delegation",
+    "filter":"(userAccountControl:1.2.840.113556.1.4.803:=524288)",
+    "properties":"*"
+    },
+    5:{
+    "name":"no kerb preauth required (asrep roast-able)",
+    "filter":"(userAccountControl:1.2.840.113556.1.4.803:=4194304)",
+    "properties":"*"
     }
 }
